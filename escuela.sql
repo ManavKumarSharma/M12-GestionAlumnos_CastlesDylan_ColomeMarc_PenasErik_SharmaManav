cb03141494fd862b5eacdd4e3c63897fd55b8f6c
Peñas Andrea <7767.joan23@fje.edu>
2024-10-08 18:19:26 +0200

    Version 2 de BBDD - Modificacion en los Inserts

--- escuela.sql
+++ escuela.sql
@@ -26,11 +26,11 @@ ALTER TABLE user ADD CONSTRAINT fk_rol_user FOREIGN KEY (id_rol) REFERENCES role
 
 INSERT INTO user VALUES 
 INSERT INTO user (id_user, nom_user, apellido_user, dni_user, fecha_nac_user, id_rol, direccion_user, telf_user, email_cole_user, email_pri_user, pwd_user, sexo_user) VALUES
-(1, 'Juan', 'Pérez', '12345678', '1980-05-15', 1, 'Av. Siempre Viva 123', '912345678', 'juan@colegio.com', 'juan.perez@gmail.com', 'pass1234', 'H'),
-(2, 'María', 'Gómez', '23456789', '1990-06-20', 2, 'Calle Falsa 456', '923456789', 'maria@colegio.com', 'maria.gomez@gmail.com', 'pass5678', 'M'),
-(3, 'Carlos', 'López', '34567890', '1985-07-25', 3, 'Calle Real 789', '934567890', 'carlos@colegio.com', 'carlos.lopez@gmail.com', 'pass9101', 'H'),
-(4, 'Ana', 'Martínez', '45678901', '2000-08-30', 4, 'Calle del Sol 321', '945678901', 'ana@colegio.com', 'ana.martinez@gmail.com', 'pass1121', 'M'),
-(5, 'Luis', 'Fernández', '56789012', '2005-09-10', 5, 'Calle Luna 654', '956789012', 'luis@colegio.com', 'luis.fernandez@gmail.com', 'pass3141', 'H');
+(1, 'Juan', 'Pérez', '12115682', '1980-05-15', 1, 'Av. Avemaria 123', '673347671', 'adminj23@fje.edu', 'juan.perez@gmail.com', 'pass1234', 'H'),
+(2, 'María', 'Gómez', '24833783', '1990-06-20', 2, 'Calle Falsa 45', '623857785', 'maria.gomez@fje.edu', 'marigomez@gmail.com', 'pass5678', 'M'),
+(3, 'Carlos', 'López', '56577126', '1985-07-25', 3, 'Calle Real 789', '666567892', 'carlos.lopez@fje.edu', 'carloopez@gmail.com', 'pass9101', 'H'),
+(4, 'Ana', 'Martínez', '43661191', '2000-08-30', 4, 'Av. Cristóbal Colón 23', '645574900', 'ana.martínez@fje.edu', 'ana.martinez@gmail.com', 'pass1121', 'M'),
+(5, 'Luis', 'Fernández', '46623090', '2005-09-10', 5, 'Av. América 14', '65699444', '10234.joan23@fje.edu', 'luisfer@gmail.com', 'pass3141', 'H');
 
 INSERT INTO roles (id_rol, tipo_rol) VALUES
 (1, 'Administrador'),

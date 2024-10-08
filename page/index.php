56e1d612eb26b82f5ca500c4b5212b701c46491a
Manav Kumar Sharma <100007272.joan23@fje.edu>
2024-10-08 17:28:53 +0200

    Añadido validaciones JavaScript al LogIn

--- page/index.php
+++ page/index.php
@@ -10,23 +10,29 @@ if (isset($_GET['errors'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="../validations/validations_js/validateLogIn.js"></script>
 </head>
 <body>
-    <form action="../validations/validations_php/validateLogIn.php" method="post">
+    <form action="../validations/validations_php/validateLogIn.php" method="post" onsubmit="validateLogInForm(event)">
         <label for="email">Correo:</label>
         <br>
-        <input type="email" name="email">
+        <input type="email" name="email" id="email">
         <br>
+        <span class="error" id="errorEmail" hidden>Por favor, ingresa un email válido</span>
         <?php
-        if (isset($errors['email'])) {echo htmlspecialchars($errors['email']) . '</br>'; } // EN CASO DE ERROR, LO MUESTRA
+        if (isset($errors['email'])) { 
+            echo '<span class="error">' . htmlspecialchars($errors['email']) . '</span><br>'; // EN CASO DE ERROR, LO MUESTRA
+        } 
         ?>
         <br>
         <label for="password">Contraseña:</label>
         <br>
-        <input type="password" name="password">
+        <input type="password" name="password" id="password">
         <br>
+        <span class="error" id="errorPassword" hidden>Por favor, ingresa una contraseña</span>
         <?php
-        if (isset($errors['password'])) {echo htmlspecialchars($errors['password']) . '</br>'; } // EN CASO DE ERROR, LO MUESTRA
+        if (isset($errors['password'])){
+            echo '<span class="error">' . htmlspecialchars($errors['password']) . '</span><br>'; } // EN CASO DE ERROR, LO MUESTRA
         ?>
         <br>
         <button type="submit" name="login">Iniciar Sesión</button>

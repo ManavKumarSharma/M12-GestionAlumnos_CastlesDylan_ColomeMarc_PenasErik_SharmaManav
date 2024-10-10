18cd7dc508532e3859bf6985f4ba53ca7a6398e5
Peñas Andrea <7767.joan23@fje.edu>
2024-10-10 17:40:32 +0200

    LoginProcess prueba 1

--- process/loginProcess.php
+++ process/loginProcess.php
@@ -1,10 +1,28 @@
 <?php
-echo "Lamepingas";
 session_start();
+
+include ("./connection/connection.php");
+
+echo $_SESSION['data']['email'];
+$_SESSION["data"][$campo];
+
+
+if(isset($_SESSION["data"])){
+    $valoresBBDD = [];
+    foreach ($_SESSION["data"] as $campo => $valor) {
+        // $valoresBBDD[$campo] = mysqli_real_escape_string($mysqli, $valor);
+        echo $valoresBBDD[$campo];
+        echo $valor;
+    }
+}
+
+
 
 $_SESSION["data"];
 
 echo $_SESSION["data"]["email"];
+
+
 
 
 

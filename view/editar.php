bd41197e3fc3290b09eac9f4e442a3ad8d4aa5e9
ErikPenyas19 <7767.joan23@fje.edu>
2024-11-27 15:56:23 +0100

    Estructura básica editar.php

---
+++ view/editar.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="loginEdit" method="post">
+        <!-- <h4>Matricula</h4><br>
+        <input type="text"><br><br> -->
+        <h4>Nombre</h4>
+        <input type="text" name="name_al"><br><br>
+
+        <h4>Apellidos</h4><br>
+        <input type="text" name="ap_al"><br><br>
+
+        <h4>NIF/NIE</h4><br>
+        <input type="text" name="nif_al"><br><br>
+
+        <h4>Nacimiento</h4><br>
+        <input type="date" name="fecha_al" id=""><br><br>
+
+        <h4>Dirección</h4><br>
+        <input type="text" name="dir_al"><br><br>
+
+        <h4>Teléfono</h4><br>
+        <input type="text" name="tel_al"><br><br>
+        
+        <h4>Email Escolar</h4><br>
+        <input type="text" name="em-esc_al"><br><br>
+
+        <h4>Email Personal</h4><br>
+        <input type="text" name="em-per_al"><br><br>
+
+        <h4>Sexo</h4><br>
+        <input type="text" name="sex_al"><br><br>
+    </form>
+    
+</body>
+</html>

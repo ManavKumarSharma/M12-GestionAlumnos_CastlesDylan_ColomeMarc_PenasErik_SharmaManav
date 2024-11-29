22d25052710a55cc80b44ed0b45f08a77cf8ed68
Marcolo24 <100005655.joan23@fje.edu>
2024-11-29 17:30:12 +0100

    crearUsuarios

--- php/añadirUsuario.php
+++ php/añadirUsuario.php
@@ -1,33 +1,52 @@
 <?php
 
-require_once 'php/connection/connection.php';
+require_once '../php/connection/connection.php';
 
-$nombre = htmlspecialchars(mysqli_real_query($_POST['name_al'], $mysql)) ?? '';
+$nombre = htmlspecialchars(mysqli_real_escape_string($mysqli,trim($_POST['name_al'])));
 
-$apellido = htmlspecialchars(mysqli_real_query($_POST['ap_al'], $mysql))?? '';
+$apellido = htmlspecialchars(mysqli_real_escape_string($mysqli,trim($_POST['ap_al'])));
 
-$dni = htmlspecialchars(mysqli_real_query($_POST['nif_al'], $mysql))?? '';
+$dni = htmlspecialchars(mysqli_real_escape_string($mysqli,trim($_POST['nif_al'])));
+ 
+$fecha = htmlspecialchars(mysqli_real_escape_string($mysqli,trim($_POST['fecha_al'])));
 
-$fecha = htmlspecialchars(mysqli_real_query($_POST['fecha_al'], $mysql))?? '';
+$direccion = htmlspecialchars(mysqli_real_escape_string($mysqli,trim($_POST['dir_al'])));
 
-$direccion = htmlspecialchars(mysqli_real_query($_POST['dir_al'], $mysql))?? '';
+$telefono = htmlspecialchars(mysqli_real_escape_string($mysqli,trim($_POST['tel_al'])));
 
-$telefono = htmlspecialchars(mysqli_real_query($_POST['tel_al'], $mysql))?? '';
+$emailColegio = htmlspecialchars(mysqli_real_escape_string($mysqli,trim($_POST['em-esc_al'])));
 
-$emailColegio = htmlspecialchars(mysqli_real_query($_POST['em-esc_al'], $mysql))?? '';
+$emailPersonal = htmlspecialchars(mysqli_real_escape_string($mysqli,trim($_POST['em-per_al'])));
 
-$emailPersonal = htmlspecialchars(mysqli_real_query($_POST['em-per_al'], $mysql))?? '';
+$sexo = htmlspecialchars(mysqli_real_escape_string($mysqli,trim($_POST['sexo_al'])));
 
-$query = "INSERT INTO tbl_alumnos (nombre_alumno, apellido_alumno, dni_alumno, fecha_nac_alumno, direccion_alumno, telf_alumno, email_cole_alumno, email_pri_alumno) VALUES ('$nombre', '$apellido', '$dni', '$fecha', '$direccion', '$telefono', '$emailColegio', '$emailPersonal')";
+$query = "INSERT INTO tbl_alumnos (nombre_alumno, apellido_alumno, dni_alumno, fecha_nac_alumno, direccion_alumno, telf_alumno, email_cole_alumno, email_pri_alumno, sexo_user) VALUES ('$nombre', '$apellido', '$dni', '$fecha', '$direccion', '$telefono', '$emailColegio', '$emailPersonal','$sexo')";
 
-$result = mysqli_query($mysql, $query);
 
-if ($result) {
-    header('Location: index.php');
-} else {
-    echo "Error: ". $query. "<br>". mysqli_error($mysql);
+
+try {
+
+
+    // echo $sqlEdit;
+    // exit();
+    // Inicializamos el stmt
+    $stmt = mysqli_stmt_init($mysqli);
+
+    // Preparamos la consulta
+    if (mysqli_stmt_prepare($stmt, $query)) {
+
+        // Ejecutamos el stmt
+        mysqli_stmt_execute($stmt);
+
+        // Obtenemos los resultados
+        $result = mysqli_stmt_get_result($stmt);
+
+        // Cerramos el stmt
+        mysqli_stmt_close($stmt);
+    }
+    header("Location: ../view/recepcion.php");
+    exit();
+} catch (Exception $e) {
+    echo "Error al crear usuario: " . $e->getMessage();
+    die();
 }
-
-mysqli_close($mysql);
-
-?>
--- view/recepcion.php
+++ view/recepcion.php
@@ -3,7 +3,7 @@
 session_start();
 
 // Verificamos si existe la variable de SESSION
-if (!isset($_SESSION['session_user'])) {  
+if (!isset($_SESSION['session_user'])) {
     header('Location: ./index.php');
     exit;
 }
@@ -25,6 +25,7 @@ $total_pages = ceil($total_records / $limit_number);
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -32,6 +33,7 @@ $total_pages = ceil($total_records / $limit_number);
     <title>Página principal</title>
     <script src="../js/recargarTabla.js"></script>
 </head>
+
 <body>
     <!-- Header de las paginas -->
     <header id="header">
@@ -39,7 +41,7 @@ $total_pages = ceil($total_records / $limit_number);
         <img id="logo-cent" src="../img/logo_fje.svg" alt="">
         <div id="btns-der">
             <?php
-                $rutaFotoPerfil = '../fotosPerfil/' . $_SESSION['session_user']['id'] . '.png';
+            $rutaFotoPerfil = '../fotosPerfil/' . $_SESSION['session_user']['id'] . '.png';
             ?>
             <div id="img-userContainer">
                 <img src="<?php echo file_exists($rutaFotoPerfil) ? $rutaFotoPerfil : '../img/profilePic.png' ?>" alt="" id="img-user">
@@ -54,27 +56,32 @@ $total_pages = ceil($total_records / $limit_number);
         <nav id="menu">
             <h1 id="tituloPequeño" class="fuenteAzulClaro">OPCIONES</h1>
             <div class="opcionCaja" id="cajaCrearAlumno">
-                <div id="parteIconoCaja">
-                    <img src="../img/simboloCrearUsuario.png" class="simboloCaja">
-                </div>
-                <div id="parteNombreCaja">
-                    <p class="fuenteBlanca" id="textoCajas">Crear usuario</p>
-                </div>
+                <a class="quitarEstilosBtn" href="crear.php">
+                    <div id="parteIconoCaja">
+                        <img src="../img/simboloCrearUsuario.png" class="simboloCaja">
+                    </div>
+                    <div id="parteNombreCaja">
+                        <p class="fuenteBlanca" id="textoCajas">Crear usuario</p>
+                    </div>
+                </a>
             </div>
             <div class="opcionCaja" id="cajaNotasMedias">
-                <div id="parteIconoCaja">
-                    <img src="../img/simboloNotasMedias.png" class="simboloCaja">
-                </div>
-                <div id="parteNombreCaja">
-                    <p class="fuenteBlanca" id="textoCajas">Notas medias</p>
-                </div>
+                <a class="quitarEstilosBtn" href="media.php">
+
+                    <div id="parteIconoCaja">
+                        <img src="../img/simboloNotasMedias.png" class="simboloCaja">
+                    </div>
+                    <div id="parteNombreCaja">
+                        <p class="fuenteBlanca" id="textoCajas">Notas medias</p>
+                    </div>
+                </a>
             </div>
         </nav>
         <div id="content-right" class="fuenteNegra">
 
-            
+
             <!-- Si no hay datos  -->
-            <?php if(mysqli_num_rows($data) == 0): ?>
+            <?php if (mysqli_num_rows($data) == 0): ?>
                 <span>No hay datos</span>
             <?php else: ?>
                 <!-- Formulario de selección de resultados por página -->
@@ -87,7 +94,7 @@ $total_pages = ceil($total_records / $limit_number);
                     </select>
                     <input type="hidden" name="resutlsbtn" value="<?php echo $page; ?>">
                 </form>
-                
+
                 <!-- Tabla con los resultados -->
                 <table id="tbl-content">
                     <thead>
@@ -117,9 +124,9 @@ $total_pages = ceil($total_records / $limit_number);
                             echo "<td>" . htmlspecialchars($row['telf_alumno']) . "</td>";
                             echo "<td class='ocultar'>" . htmlspecialchars($row['email_cole_alumno']) . "</td>";
                             echo "<td class='ocultar'>" . htmlspecialchars($row['email_pri_alumno']) . "</td>";
-                            echo "<td class='ocultar'>" . htmlspecialchars($row['sexo_user']) . "</td>";                        
-                            echo "<td class='celdaEditar' class='editarBoton' data-id='" . $row['matricula_alumno'] . "'><img id='iconoEditar' src='../img/iconoEditar.png'></td>";  
-                            echo "<td class='celdaEliminar' class='eliminarBoton' data-id='" . $row['matricula_alumno'] . "'><img id='iconoEliminar' src='../img/eliminarIcono.png'></td>";  
+                            echo "<td class='ocultar'>" . htmlspecialchars($row['sexo_user']) . "</td>";
+                            echo "<td class='celdaEditar' class='editarBoton' data-id='" . $row['matricula_alumno'] . "'><img id='iconoEditar' src='../img/iconoEditar.png'></td>";
+                            echo "<td class='celdaEliminar' class='eliminarBoton' data-id='" . $row['matricula_alumno'] . "'><img id='iconoEliminar' src='../img/eliminarIcono.png'></td>";
                             echo "</tr>";
                         }
                         ?>
@@ -127,25 +134,27 @@ $total_pages = ceil($total_records / $limit_number);
                 </table>
             <?php endif; ?>
             <?php if ($total_pages > 1): ?>
-                    <div class="pagination">
+                <div class="pagination">
                     <span>Página <?php echo $page; ?> de <?php echo $total_pages; ?></span>
-                        <br><p></p>
-                        <!-- Enlaces de paginación -->
-                        <?php if ($page > 1): ?>
-                            <a class="noEstilosLink" href="?page=1&results=<?php echo $limit_number; ?>">Primera</a>
-                            <a class="noEstilosLink" href="?page=<?php echo $page - 1; ?>&results=<?php echo $limit_number; ?>">Anterior</a>
-                        <?php endif; ?>
+                    <br>
+                    <p></p>
+                    <!-- Enlaces de paginación -->
+                    <?php if ($page > 1): ?>
+                        <a class="noEstilosLink" href="?page=1&results=<?php echo $limit_number; ?>">Primera</a>
+                        <a class="noEstilosLink" href="?page=<?php echo $page - 1; ?>&results=<?php echo $limit_number; ?>">Anterior</a>
+                    <?php endif; ?>
 
 
 
-                        <?php if ($page < $total_pages): ?>
-                            <a class="noEstilosLink" href="?page=<?php echo $page + 1; ?>&results=<?php echo $limit_number; ?>">Siguiente</a>
-                            <a class="noEstilosLink" href="?page=<?php echo $total_pages; ?>&results=<?php echo $limit_number; ?>">Última</a>
-                        <?php endif; ?>
-                    </div>
-                <?php endif; ?>
+                    <?php if ($page < $total_pages): ?>
+                        <a class="noEstilosLink" href="?page=<?php echo $page + 1; ?>&results=<?php echo $limit_number; ?>">Siguiente</a>
+                        <a class="noEstilosLink" href="?page=<?php echo $total_pages; ?>&results=<?php echo $limit_number; ?>">Última</a>
+                    <?php endif; ?>
+                </div>
+            <?php endif; ?>
         </div>
     </main>
     <script src="../js/botonesAcciones.js"></script>
 </body>
+
 </html>
